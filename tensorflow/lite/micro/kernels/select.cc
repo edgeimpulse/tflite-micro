4b4af38f3d55b7d401f5f5fa53322a393b95b714
Dmitry Maslov <dmitrywat@gmail.com>
2023-03-11 18:14:53 +0100

    fixed paths for new ops and re-added register select v1

--- tensorflow/lite/micro/kernels/select.cc
+++ tensorflow/lite/micro/kernels/select.cc
@@ -12,16 +12,16 @@ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License.
 ==============================================================================*/
-#include "tensorflow/lite/kernels/internal/reference/select.h"
+#include "edge-impulse-sdk/tensorflow/lite/kernels/internal/reference/select.h"
 
 #include <stddef.h>
 #include <stdint.h>
 
-#include "tensorflow/lite/c/common.h"
-#include "tensorflow/lite/kernels/internal/tensor_ctypes.h"
-#include "tensorflow/lite/kernels/kernel_util.h"
-#include "tensorflow/lite/micro/kernels/kernel_util.h"
-#include "tensorflow/lite/micro/micro_log.h"
+#include "edge-impulse-sdk/tensorflow/lite/c/common.h"
+#include "edge-impulse-sdk/tensorflow/lite/kernels/internal/tensor_ctypes.h"
+#include "edge-impulse-sdk/tensorflow/lite/kernels/kernel_util.h"
+#include "edge-impulse-sdk/tensorflow/lite/micro/kernels/kernel_util.h"
+#include "edge-impulse-sdk/tensorflow/lite/micro/micro_log.h"
 
 namespace tflite {
 
@@ -29,6 +29,11 @@ constexpr int kInputTensorCondition = 0;
 constexpr int kInputTensorX = 1;
 constexpr int kInputTensorY = 2;
 constexpr int kOutputTensor = 0;
+
+enum KernelType {
+  kVersionOne,
+  kVersionTwo,
+};
 
 struct OpData {
   bool requires_broadcast;
@@ -75,52 +80,64 @@ TfLiteStatus CheckBroadcastShape(TfLiteContext* context,
   return kTfLiteOk;
 }
 
+template <KernelType kernel_type>
 TfLiteStatus SelectPrepare(TfLiteContext* context, TfLiteNode* node) {
   OpData* data = reinterpret_cast<OpData*>(node->user_data);
 
   TF_LITE_ENSURE_EQ(context, NumInputs(node), 3);
   TF_LITE_ENSURE_EQ(context, NumOutputs(node), 1);
 
-  MicroContext* micro_context = GetMicroContext(context);
-  TfLiteTensor* input_condition =
-      micro_context->AllocateTempInputTensor(node, kInputTensorCondition);
-
-  TfLiteTensor* input_x =
-      micro_context->AllocateTempInputTensor(node, kInputTensorX);
-
-  TfLiteTensor* input_y =
-      micro_context->AllocateTempInputTensor(node, kInputTensorY);
-
-  TfLiteTensor* output =
-      micro_context->AllocateTempOutputTensor(node, kOutputTensor);
+  const TfLiteTensor* input_condition;
+  TF_LITE_ENSURE_OK(context, GetInputSafe(context, node, kInputTensorCondition,
+                                          &input_condition));
+  const TfLiteTensor* input_x;
+  TF_LITE_ENSURE_OK(context,
+                    GetInputSafe(context, node, kInputTensorX, &input_x));
+  const TfLiteTensor* input_y;
+  TF_LITE_ENSURE_OK(context,
+                    GetInputSafe(context, node, kInputTensorY, &input_y));
+  TfLiteTensor* output;
+  TF_LITE_ENSURE_OK(context,
+                    GetOutputSafe(context, node, kOutputTensor, &output));
 
   // Input must be bool.
   TF_LITE_ENSURE_TYPES_EQ(context, input_condition->type, kTfLiteBool);
   TF_LITE_ENSURE_TYPES_EQ(context, input_x->type, input_y->type);
   output->type = input_x->type;
 
-  // Respect the original output shape when there are mixed shapes to represent
-  // a scalar data.
-  if (GetTensorShape(input_condition).FlatSize() == 1 &&
-      GetTensorShape(input_x).FlatSize() == 1 &&
-      GetTensorShape(input_y).FlatSize() == 1 &&
-      GetTensorShape(output).FlatSize() == 1) {
-    return kTfLiteOk;
-  }
-
   bool same_shape = HaveSameShapes(input_condition, input_x) &&
                     HaveSameShapes(input_x, input_y);
+  TfLiteIntArray* output_size;
   if (!same_shape) {
-    TF_LITE_ENSURE_OK(
-        context, CheckBroadcastShape(context, input_condition, input_x, input_y,
-                                     output->dims));
-    data->requires_broadcast = true;
-  }
+    switch (kernel_type) {
+      case kVersionOne: {
+        bool is_input_condition_scalar = NumDimensions(input_condition) == 0;
+        bool has_rank_one_input_condition =
+            NumDimensions(input_condition) == 1 &&
+            SizeOfDimension(input_condition, 0) == SizeOfDimension(input_x, 0);
+        data->has_low_rank_input_condition =
+            is_input_condition_scalar || has_rank_one_input_condition;
+        TF_LITE_ENSURE(context, data->has_low_rank_input_condition);
 
-  micro_context->DeallocateTempTfLiteTensor(input_condition);
-  micro_context->DeallocateTempTfLiteTensor(input_x);
-  micro_context->DeallocateTempTfLiteTensor(input_y);
-  micro_context->DeallocateTempTfLiteTensor(output);
+        output_size = TfLiteIntArrayCopy(input_x->dims);
+
+        // Input tensors must have the same type and size
+        TF_LITE_ENSURE(context, HaveSameShapes(input_x, input_y));
+        break;
+      }
+      case kVersionTwo: {
+        TF_LITE_ENSURE_OK(context, CalculateShapeForBroadcast(
+                                       context, input_condition, input_x,
+                                       input_y, &output_size));
+        data->requires_broadcast = true;
+        break;
+      }
+      default:
+        return kTfLiteError;
+    }
+  } else {
+    output_size = TfLiteIntArrayCopy(input_x->dims);
+  }
 
   return kTfLiteOk;
 }
@@ -184,13 +201,18 @@ TfLiteStatus SelectEval(TfLiteContext* context, TfLiteNode* node) {
   return kTfLiteOk;
 }
 
+TfLiteRegistration Register_SELECT() {
+  return tflite::micro::RegisterOp(tflite::SelectInit, tflite::SelectPrepare<kVersionOne>,
+                                   tflite::SelectEval);
+}
+
 // SelectV2 op selects values of 'x' if the corresponding value of 'condition'
 // is true or the value of 'y' if false. There are valid condition input sizes:
 //
 // 1. Either the same shape (in which case the select is elementwise), or
 // 2. Broadcastable shapes between 'condition', 'x' and 'y'.
 TfLiteRegistration Register_SELECT_V2() {
-  return tflite::micro::RegisterOp(tflite::SelectInit, tflite::SelectPrepare,
+  return tflite::micro::RegisterOp(tflite::SelectInit, tflite::SelectPrepare<kVersionTwo>,
                                    tflite::SelectEval);
 }
 
